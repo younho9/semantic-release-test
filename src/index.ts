d3f92ae817c614b92578e028eafc109f691ea233
Younho Choo <younho9.choo@gmail.com>
2021-09-06 23:02:48 +0900

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export const sum = (a: number, b: number): number => a + b;
+
+export const subtract = (a: number, b: number): number => a - b;
+
+export const multiple = (a: number, b: number): number => a * b;
