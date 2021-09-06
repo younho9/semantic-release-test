bc57782abd8eb1eee54c356ab6a8927aa2b8439f
Younho Choo <younho9.choo@gmail.com>
2021-09-06 23:20:40 +0900

    feat: add divide func

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export const sum = (a: number, b: number): number => a + b;
 export const subtract = (a: number, b: number): number => a - b;
 
 export const multiple = (a: number, b: number): number => a * b;
+
+export const divide = (a: number, b: number): number => a / b;
