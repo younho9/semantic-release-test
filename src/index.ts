bf8776cd9f28f0c97e8ee80942ad23acb149e70c
Younho Choo <younho9.choo@gmail.com>
2021-09-06 23:34:40 +0900

    feat: add concat

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,6 @@ export const subtract = (a: number, b: number): number => a - b;
 export const multiple = (a: number, b: number): number => a * b;
 
 export const divide = (a: number, b: number): number => a / b;
+
+export const concat = (strLeft: string, strRight: string): string =>
+  `${strLeft}${strRight}`;
